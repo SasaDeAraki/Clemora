0729be898a665b5c5393f6c2b53102f7729c6424
Samuel Viana <samuel.viana.yl.gmail.com>
2024-06-05 18:33:27 -0300

    Adição do botão "mostrar/ocultar" e suas funcionalidades na página "Resolução"

---
+++ js/texto-oculto.js
@@ -0,0 +1,11 @@
+document.getElementById('mostrar-texto').addEventListener('click', function() {
+    let botao = document.getElementById('mostrar-texto')
+    let text = document.getElementById('texto-oculto');
+    if (text.style.display === 'none') {
+        text.style.display = 'block';
+        botao.innerHTML = '^';
+    } else {
+        text.style.display = 'none';
+        botao.innerHTML = 'v';
+    }
+});
