d8b2bfeb8f109aba46605377c3636c7ddb91f303
Samuel Viana <samuel.viana.yl.gmail.com>
2024-06-06 14:53:51 -0300

    Finalização da página "FAQ"

--- js/resposta-faq.js
+++ js/resposta-faq.js
@@ -1,35 +1,14 @@
-document.getElementById('faq-1').addEventListener('click', function() {
-    let resposta = document.getElementById('resp-1');
-    let botao = document.getElementById('b-1');
-    if (resposta.style.display === 'none') {
-        resposta.style.display = 'block';
-        botao.innerHTML = '^'
-    } else {
-        resposta.style.display = 'none';
-        botao.innerHTML = 'v'
-    }
-})
-
-document.getElementById('faq-2').addEventListener('click', function() {
-    let resposta = document.getElementById('resp-2');
-    let botao = document.getElementById('b-2');
-    if (resposta.style.display === 'none') {
-        resposta.style.display = 'block';
-        botao.innerHTML = '^'
-    } else {
-        resposta.style.display = 'none';
-        botao.innerHTML = 'v'
-    }
-})
-
-document.getElementById('faq-3').addEventListener('click', function() {
-    let resposta = document.getElementById('resp-3');
-    let botao = document.getElementById('b-3');
-    if (resposta.style.display === 'none') {
-        resposta.style.display = 'block';
-        botao.innerHTML = '^'
-    } else {
-        resposta.style.display = 'none';
-        botao.innerHTML = 'v'
-    }
-})
+document.querySelectorAll('.container-faq div').forEach(div => {
+    div.addEventListener('click', function() {
+        let index = this.getAttribute('data-index');
+        let resposta = document.getElementById('resp-' + index);
+        let button = document.getElementById('b-' + index);
+        if (resposta.style.display === 'none' || resposta.style.display === '') {
+            resposta.style.display = 'block';
+            button.innerHTML = '^';
+        } else {
+            resposta.style.display = 'none';
+            button.innerHTML = 'v';
+        }
+    });
+});
